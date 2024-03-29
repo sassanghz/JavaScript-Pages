6710b1df00a1ae807761af4879a1fd8a88f80067
sassanghz <sassanghazi@gmail.com>
2024-03-29 16:20:23 -0600

    reformatted html

--- Exercise 6/Exercise6.js
+++ Exercise 6/Exercise6.js
@@ -1,7 +1,47 @@
 let slideIndex = 1;
 showSlides(slideIndex);
 
-function plusSlides(n){
+function plusSlides(n) {
     showSlides(slideIndex += n);
 }
 
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("slide");
+    let dots = document.getElementsByClassName("dot");
+
+    if (n > slides.length) {
+        slideIndex = 1;
+    }
+
+    if (n < 1) {
+        slideIndex = slides.length;
+    }
+
+    // Hide all slides
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+
+    // Remove active class from all dots
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+
+    // Display the current slide and add active class to its corresponding dot
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+}
+
+// Next and Previous button functionality
+document.getElementById("next").addEventListener("click", function () {
+    plusSlides(1);
+});
+
+document.getElementById("previous").addEventListener("click", function () {
+    plusSlides(-1);
+});

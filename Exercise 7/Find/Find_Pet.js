adb394423f7cff32c93a5ba2b5e050057862a5bb
sassanghz <sassanghazi@gmail.com>
2024-03-31 15:14:17 -0400

    adjusted js for validation

--- Exercise 7/Find/Find_Pet.js
+++ Exercise 7/Find/Find_Pet.js
@@ -1,14 +1,36 @@
-function validateForm(){
-    
-   var animalType = document.getElementById('animalType').value;
-   var breed = document.getElementById('breed').value;
-   var age = document.getElementById('age').value;
-   var gender = document.getElementById('gender').value;
+// Date: 2024/03/31
+// Author: Sassan Ghazi
+function validateForm() {
+    var animalType = document.getElementById('animalType').value;
+    var breed = document.getElementById('breed').value;
+    var age = document.getElementById('age').value;
+    var gender = document.getElementById('gender').value;
 
-   if(animalType === '' || breed === '' || age === '' || gender === ''){
-    alert("Please fill in all required fields.");
-    return false;
-   }
+    if (animalType === '') {
+        alert('Please select an option for Cat or Dog.');
+        return false;
+    }
 
-   return true;
+    if (breed === '') {
+        alert('Please enter the breed.');
+        return false;
+    }
+
+    if (age === '') {
+        alert('Please select an option for Preferred Age.');
+        return false;
+    }
+
+    if (gender === '') {
+        alert('Please select an option for Preferred Gender.');
+        return false;
+    }
+
+    // Use the form data for further processing or submission
+    console.log('Animal Type:', animalType);
+    console.log('Breed:', breed);
+    console.log('Age:', age);
+    console.log('Gender:', gender);
+
+    return true;
 }

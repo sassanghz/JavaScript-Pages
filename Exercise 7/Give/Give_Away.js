f29e7e74cbde0265126b6bd5e8e270436d2df53e
sassanghz <sassanghazi@gmail.com>
2024-03-31 16:17:09 -0400

    added js to give folder

--- Exercise 7/Give/Give_Away.js
+++ Exercise 7/Give/Give_Away.js
@@ -7,6 +7,8 @@ function validationForm(){
     var compatibleDogs = document.getElementById('compatibilityDogs').checked;
     var compatibilityCats = document.getElementById('compatibilityCats').checked;
     var compatibilityChildren = document.getElementById('compatibilityChildren').checked;
+    var ownerName = document.getElementById('ownerName').value;
+    var ownerEmail = document.getElementById('ownerEmail').value;
 
     if (animalType === '') {
         alert('Please select an option for Cat or Dog.');
@@ -28,11 +30,23 @@ function validationForm(){
         return false;
     }
 
+    if(ownerName === ''){
+        alert('Please give a name for the Owners Name.');
+        return false;
+    }
+
+    if(ownerEmail === ''){
+        alert('Please provide the email of the Owner.')
+        return false;
+    }
+
     // Use the form data for further processing or submission
     console.log('Animal Type:', animalType);
     console.log('Breed:', breed);
     console.log('Age:', age);
     console.log('Gender:', gender);
+    console.log('Owners Name:', ownerName);
+    console.log('Owners Email:', ownerEmail);
 
     return true;
 }
